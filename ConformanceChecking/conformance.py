ae660242327c4a4ccc35606ad066698a4e90b15b
simon <yconsj@gmail.com>
2022-11-20 13:23:31 +0100

    import engine

--- ConformanceChecking/conformance.py
+++ ConformanceChecking/conformance.py
@@ -2,16 +2,7 @@ from __future__ import division
 import json
 import xml.etree.ElementTree as ET
 import sys
-
-class cclass():
-    def add_event(self,x):
-        print("event added")
-
-    def add_relation(self,y,z,q):
-        pass
-
-    def execute(self,a):
-        return True
+import Engine
 
 
 def create_nodes(jsongraph, p):
@@ -22,28 +13,30 @@ def create_nodes(jsongraph, p):
         t = edge['target']
         nodes.add(t)
         nodes.add(s)
-    
+
     for x in nodes:
         p.add_event(x)
+
 
 def create_edges(jsongraph, p):
     for edge in jsongraph:
         s = edge['source']
         t = edge['target']
         r = edge['type']
-        p.add_relation(s,r,t)
+        p.add_relation(s, r, t)
+
 
 def create_graph(jsongraph, p):
     create_nodes(jsongraph, p)
     create_edges(jsongraph, p)
-    
+
 
 def DCR_graph(jsonfile):
     f = open(jsonfile).read()
     file = json.loads(f)
 
     jsongraph = file['Relation']
-    p = cclass()
+    p = Engine.Process()
     create_graph(jsongraph, p)
 
     return p
@@ -55,19 +48,21 @@ def read_log_from_XES(filename):
     root = tree.getroot()
 
     for child in root:
-        events= []
+        events = []
 
         for trace in child:
-            case_id=trace.attrib.get('value')
-            
-            if case_id==None:
+            case_id = trace.attrib.get('value')
+
+            if case_id == None:
                 key = trace[1].attrib.get('value')
                 events.append(key)
-                dict.append(events)                               
+                dict.append(events)
     return dict
 
 # Assumption - *When* the trace fails has an impact on *how* conforming it is
-def progress_based_conf(traces,p):
+
+
+def progress_based_conf(traces, p):
     nr_traces = len(traces)
     # Do some conformance checking
     percent_executed = 0
@@ -82,12 +77,14 @@ def progress_based_conf(traces,p):
             else:
                 nr_executed += 1
         percent_executed += nr_executed/len(trace)
-        
+
     conf_val = (percent_executed/nr_traces)
     return conf_val
 
 # Simple value. Assumption - Trace fails = no conformance
-def simple_conf(traces,p):
+
+
+def simple_conf(traces, p):
     fails = 0
     nr_traces = len(traces)
     # Do some conformance checking
@@ -97,30 +94,30 @@ def simple_conf(traces,p):
             # C# code
             fired = p.execute(event)
             if not fired:
-                fails+=1
+                fails += 1
                 break
     conf_val = 1.0-(fails/nr_traces)
     return conf_val
 
+
 def main():
     args = sys.argv[1:]
-    #Create DCR graph
-    p = DCR_graph("/home/frida/Dokumenter/E22/02269_ProcessMining/Discover/DisCoveR/logs/morten.txt.JSON")
-    #p = DCR_graph(args[0]) # As argument on commandline
+    # Create DCR graph
+    p = DCR_graph(
+        "/home/frida/Dokumenter/E22/02269_ProcessMining/Discover/DisCoveR/logs/morten.txt.JSON")
+    # p = DCR_graph(args[0]) # As argument on commandline
 
-    #Create traces from xes file
-    #traces = read_log_from_XES(args[1]) # As argument on commandline
-    traces = read_log_from_XES('/home/frida/Dokumenter/E22/02269_ProcessMining/Discover/DisCoveR/logs/morten.xes')
-    
-    simple_conf_val = simple_conf(traces,p)
+    # Create traces from xes file
+    # traces = read_log_from_XES(args[1]) # As argument on commandline
+    traces = read_log_from_XES(
+        '/home/frida/Dokumenter/E22/02269_ProcessMining/Discover/DisCoveR/logs/morten.xes')
+
+    simple_conf_val = simple_conf(traces, p)
     print("Simple conformance value: ", simple_conf_val)
 
     progress_based_conf_val = progress_based_conf(traces, p)
     print("Progress based conformance value: ", progress_based_conf_val)
 
 
-
 if __name__ == "__main__":
     main()
-
-
