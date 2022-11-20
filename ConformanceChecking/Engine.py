6ecca9542cf8e5fe4bb0114a8c6fc343df0378f8
simon <yconsj@gmail.com>
2022-11-20 13:21:45 +0100

    move conformance.py

--- ConformanceChecking/Engine.py
+++ ConformanceChecking/Engine.py
@@ -1,72 +1,64 @@
 class Relation:
-  src = '';
-  tgt = '';
-  rel = '';
+    src = ''
+    tgt = ''
+    rel = ''
 
-  def __init__(self, src, rel, tgt):
-    self.src = src;
-    self.rel = rel;
-    self.tgt = tgt;
+    def __init__(self, src, rel, tgt):
+        self.src = src
+        self.rel = rel
+        self.tgt = tgt
 
 
 class Process:
-  events = set();
-  relations = set();
+    events = set()
+    relations = set()
 
-  executed = set();
-  included = set();
-  pending = set();
+    executed = set()
+    included = set()
+    pending = set()
 
-  def add_event(self, name):
-    self.events.add(name);
-    self.included.add(name);
-    
-  def add_relation(self, src, arr, tgt):
-    self.relations.add(Relation(src, arr, tgt));
+    def add_event(self, name):
+        self.events.add(name)
+        self.included.add(name)
 
+    def add_relation(self, src, arr, tgt):
+        self.relations.add(Relation(src, arr, tgt))
 
+    def enabled(self):
+        result = set(self.events)
 
-  def enabled(self):
-    result = set(self.events)
-    
-    for r in self.relations:
-        if r.rel == "condition":
-            if r.src in self.included and not r.src in self.executed:
-                result.remove(r.tgt)
-            break
-        elif r.rel == "milestone":
-            if r.src in self.included and r.src in self.pending:
-                result.remove(r.tgt)
-            break
-        else:
-            break
+        for r in self.relations:
+            if r.rel == "condition":
+                if r.src in self.included and not r.src in self.executed:
+                    result.remove(r.tgt)
+                break
+            elif r.rel == "milestone":
+                if r.src in self.included and r.src in self.pending:
+                    result.remove(r.tgt)
+                break
+            else:
+                break
 
+        return result
 
-    return result
-    
-  def execute(self, e):
-    if e in self.pending:
-        self.pending.remove(e);
-    self.executed.add(e);
-    
-    for r in self.relations:
-      if r.rel != e:
-        continue;
+    def execute(self, e):
+        if e in self.pending:
+            self.pending.remove(e)
+        self.executed.add(e)
 
-      if r.rel == 'exclude':
-        self.included.remove(r.tgt);
-      elif r.rel == "include":
-        self.included.add(r.tgt);
-      elif r.rel == "response":
-        self.pending.add(r.tgt);
+        for r in self.relations:
+            if r.rel != e:
+                continue
 
-        
+            if r.rel == 'exclude':
+                self.included.remove(r.tgt)
+            elif r.rel == "include":
+                self.included.add(r.tgt)
+            elif r.rel == "response":
+                self.pending.add(r.tgt)
 
-
-
-  def is_accepting(self):
-    for e in self.pending:
-        if e in self.included:
-            return False
-    return True
-        
+    def is_accepting(self):
+        for e in self.pending:
+            if e in self.included:
+                return False
+        return True
--- ConformanceChecking/test.py
+++ ConformanceChecking/test.py
@@ -1,70 +1,79 @@
 import Engine
+
+
 def Show(P):
     print("    Enabled: {}".format(P.enabled()))
     print("    Accepting: {}".format(P.is_accepting()))
     print("    Included: {}".format(P.included))
+
+
 def Execute(P, e):
     print("Executing {}".format(e))
     P.execute(e)
     Show(P)
-def Header(P,title):
+
+
+def Header(P, title):
     print("* New graph: {}".format(title))
     Show(P)
+
 
 def main():
     print("DCR Process Test")
 
-    #Construct A -->* B
+    # Construct A -->* B
     P = Engine.Process()
     P.add_event("A")
     P.add_event("B")
-    P.add_relation("A", "condition","B")
+    P.add_relation("A", "condition", "B")
 
     Header(P, "A-->* B")
-    Execute(P,"A")
+    Execute(P, "A")
 
-    #Construct A *--> B
+    # Construct A *--> B
     P = Engine.Process()
     P.add_event("A")
     P.add_event("B")
-    P.add_relation("A", "response","B")
+    P.add_relation("A", "response", "B")
 
     Header(P, "A *--> B")
-    Execute(P,"A")
-    Execute(P,"B")
+    Execute(P, "A")
+    Execute(P, "B")
 
     # Construct process A-->% B-->* C
-    P = Engine.Process();
-    P.add_event("A");
-    P.add_event("B");
-    P.add_event("C");
-    P.add_relation("A", "exclude", "B");
-    P.add_relation("B", "condition", "C");
+    P = Engine.Process()
+    P.add_event("A")
+    P.add_event("B")
+    P.add_event("C")
+    P.add_relation("A", "exclude", "B")
+    P.add_relation("B", "condition", "C")
 
-    Header(P, "A -->% B -->* C");
+    Header(P, "A -->% B -->* C")
     Execute(P, "A")
 
     # construct process A -->% !B --<> C
-    P = Engine.Process();
-    P.add_event("A");
-    P.add_event("B");
-    P.pending.add("B");
-    P.add_event("C");
-    P.add_relation("A", "exclude", "B");
-    P.add_relation("B", "milestone", "C");
+    P = Engine.Process()
+    P.add_event("A")
+    P.add_event("B")
+    P.included.remove("B")
+    P.pending.add("B")
+    P.add_event("C")
+    P.add_relation("A", "exclude", "B")
+    P.add_relation("B", "milestone", "C")
 
-    Header(P, "A -->% !B --<> C");
-    Execute(P, "A");
+    Header(P, "A -->% !B --<> C")
+    Execute(P, "A")
 
     # construct process A -->+ %B
-    P = Engine.Process();
-    P.add_event("A");
-    P.add_event("B");
-    P.add_relation("A", "include", "B");
-    P.included.remove("B");
+    P = Engine.Process()
+    P.add_event("A")
+    P.add_event("B")
+    P.add_relation("A", "include", "B")
+    P.included.remove("B")
 
-    Header(P, "A -->+ %B");
-    Execute(P, "A");
+    Header(P, "A -->+ %B")
+    Execute(P, "A")
+
 
 if __name__ == "__main__":
     main()
