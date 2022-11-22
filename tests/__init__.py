cf8008d60cc23995a1735f38332b32b08de181a6
s194581 <s194581@student.dtu.dk>
2022-11-22 19:55:52 +0100

    Removed unnecessary files, added results generator and master thesis results

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-"""
-Used to make the execution of the test as a package possible
-"""
-import os
-import sys
-
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../conformancechecker'))
-sys.path.insert(0, path)
